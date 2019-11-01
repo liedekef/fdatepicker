55fca3ef64910d7439f4223ea215d564a68a5c7c
Franky Van Liedekerke <liedekef@localhost.localdomain>
2019-11-01 14:09:59 +0100

    more renaming

--- README.md
+++ README.md
@@ -1,28 +1,27 @@
-# Air Datepicker
+# fDatepicker
 
 Lightweight customizable cross-browser jQuery datepicker, built with es5 and css-flexbox. Works in all modern desktop and mobile browsers (tested on Android 4.4+ and iOS8+).
 
-![air datepicker image](https://github.com/t1m0n/air-datepicker/raw/master/docs/img/promo-img-time.png)
+![fdatepicker image](https://github.com/t1m0n/air-datepicker/raw/master/docs/img/promo-img-time.png)
 
 ## Install
 
 ### bower
 ```
-bower i --save air-datepicker
+bower i --save fdatepicker
 ```
 ### npm
 ```
-npm i --save air-datepicker
+npm i --save fdatepicker
 ```
 
 ## Usage
 ```javascript
-$('.my-datepicker').datepicker([options])
+$('.my-datepicker').fdatepicker([options])
 ```
 
 ## Demo and docs
 * [In English](http://t1m0n.name/air-datepicker/docs/)
-* [In Russian](http://t1m0n.name/air-datepicker/docs/index-ru.html)
 
 ## Change log
 
--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ gulp.task('css', require('./tasks/css'));
 gulp.task('js', require('./tasks/js'));
 gulp.task('i18n', require('./tasks/i18n'));
 gulp.task('cssPage', require('./tasks/cssPage'));
-gulp.task('jade-ru', require('./tasks/jade').ru);
 gulp.task('jade-en', require('./tasks/jade').en);
 gulp.task('gzip', require('./tasks/gzip'));
 
@@ -25,13 +24,13 @@ gulp.task('watch', function () {
         livereload.changed(file)
     });
 
-    gulp.watch('docs/jade/**/*.jade', ['jade-ru', 'jade-en']).on('change', function (file) {
+    gulp.watch('docs/jade/**/*.jade', ['jade-en']).on('change', function (file) {
         livereload.changed(file)
     });
 });
 
-gulp.task('dev', ['css','js','i18n', 'cssPage', 'jade-ru', 'jade-en', 'watch']);
-gulp.task('build', ['css','js','i18n', 'jade-ru', 'jade-en']);
+gulp.task('dev', ['css','js','i18n', 'cssPage', 'jade-en', 'watch']);
+gulp.task('build', ['css','js','i18n', 'jade-en']);
 
 
 
